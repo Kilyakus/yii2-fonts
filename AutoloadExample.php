2292ee4c49a5bc4cac621d74d651657713b6f720
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-08-18 03:11:32 +0500

    Initial commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace kilyakus\fonts;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
