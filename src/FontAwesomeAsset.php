a56258699741a7c51012fadaf691d7ceeba08879
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-08-18 04:49:40 +0500

    Initial commit

--- src/FontAwesomeAsset.php
+++ src/FontAwesomeAsset.php
@@ -5,7 +5,7 @@ class FontAwesomeAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@bower/font-awesome';
     public $css = [
-        'css/font-awesome.min.css',
+        'css/all.css',
     ];
     public $jsOptions = array(
         'position' => \yii\web\View::POS_HEAD
