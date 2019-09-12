92a43b6974f8adc608599c40c672c6f4e7302279
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-09-12 17:57:01 +0500

    Initial commit

--- src/FontAwesomeAsset.php
+++ src/FontAwesomeAsset.php
@@ -3,9 +3,13 @@ namespace kilyakus\fonts;
 
 class FontAwesomeAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/font-awesome';
+    // public $sourcePath = '@bower/font-awesome';
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/vendors/font-awesome';
+    }
     public $css = [
-        'css/all.css',
+        'css/font-awesome.css',
     ];
     public $jsOptions = array(
         'position' => \yii\web\View::POS_HEAD
